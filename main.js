19a3ac8513aa7d693e27d22f82a88558ba0947b7
johnsmccoy <johnsmccoy@comcast.net>
2018-04-09 11:57:52 -0500

    all local storage on chrome

---
+++ main.js
@@ -0,0 +1,7 @@
+// Define the database as an object
+const HomeInventoryDatabase = {}
+
+// Define the arrays (a.k.a. tables) in the database to store each type of items
+HomeInventoryDatabase.furniture = []
+HomeInventoryDatabase.crafts = []
+HomeInventoryDatabase.electronics = []
